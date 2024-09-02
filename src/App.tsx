f78458a07257c677b408309e245115441b7538b5
RealToma <real.toma.contact@gmail.com>
2024-09-02 11:35:09 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { CryptoInnovationPage } from "./components/CryptoInnovationPage";
+// import { MainPage } from "./components/MainPage";
+
+function App() {
+  return (
+    <>
+      {/* <MainPage /> */}
+      <CryptoInnovationPage />
+    </>
+  );
+}
+
+export default App;
