3bf3e5cdbb260738e13e1ca7307ac7bf7d18ce43
RealToma <real.toma.contact@gmail.com>
2024-09-02 12:40:13 -0400

    updated deposit button

--- src/components/CryptoInnovationPage.tsx
+++ src/components/CryptoInnovationPage.tsx
@@ -36,7 +36,7 @@ export const CryptoInnovationPage = () => {
   const [solAmount, setSolAmount] = useState(0);
 
   const handleAmountClick = (amount: number) => {
-    setSolAmount(amount);
+    setSolAmount((prevVal) => amount + prevVal);
   };
 
   const handleReset = () => {
@@ -216,7 +216,9 @@ export const CryptoInnovationPage = () => {
                     </button>
                   </div>
                   <div
-                    className={`relative inline-block p-[2px] w-full rounded-full`}
+                    className={`relative inline-block p-[2px] w-full rounded-full ${
+                      solAmount === 0 ? "opacity-30" : ""
+                    }`}
                     style={{
                       background: "linear-gradient(to right, #623EFF, #7AD6FF)", // Gradient border background
                       borderRadius: "6px" // Ensures the outer wrapper is fully rounded
