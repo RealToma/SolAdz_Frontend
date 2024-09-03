414c093191b75ad4e3f6bf39ceff10977559f3fc
RealToma <real.toma.contact@gmail.com>
2024-09-03 11:52:45 -0400

    added button animation and updated modal

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import SignatureRequestModal from "./SignatureRequestModal";
+import { SignatureRequestModal } from "./SignatureRequestModal";
 import CalculatorBg from "../assets/calculator-bg.png";
 
 export const Calculator = () => {
@@ -12,6 +12,8 @@ export const Calculator = () => {
   const handleReset = () => {
     setSolAmount(0);
   };
+
+  const availableValues = [0.1, 0.3, 0.5, 1, 1.5, 3, 5, 10, 25, 55, 50, 100];
 
   return (
     <div className="flex items-center justify-center lg:block ">
@@ -34,7 +36,7 @@ export const Calculator = () => {
               </div>
             </div>
             <div className="grid grid-cols-4 gap-3 md:gap-5 mb-4">
-              {[0.1, 0.3, 0.5, 1, 1.5, 3, 5, 10, 25, 55, 50, 100].map((amount) => (
+              {availableValues.map((amount) => (
                 <>
                   <div
                     className={`relative bg-button-gradient-2 inline-block rounded-[6px] p-[2px] `}
@@ -42,11 +44,7 @@ export const Calculator = () => {
                     <button
                       onClick={() => handleAmountClick(amount)}
                       className={`
-          relative bg-[#191e34] rounded-full px-1 sm:px-2 w-full py-1 text-white font-light text-sm 
-           flex items-center justify-center text-nowrap`}
-                      style={{
-                        borderRadius: "6px" // Ensures the button itself is fully rounded
-                      }}
+          relative bg-[#191e34] hover:brightness-150 transition-all duration-300 px-1 sm:px-2 w-full py-1 text-white font-light text-sm flex items-center justify-center text-nowrap rounded-[6px]`}
                     >
                       +{amount} SOL
                     </button>
@@ -60,7 +58,7 @@ export const Calculator = () => {
               >
                 <button
                   onClick={handleReset}
-                  className="w-full bg-[#140e3c] text-white py-2 px-4 rounded flex-1 font-medium text-[15px]"
+                  className="w-full bg-[#140e3c] hover:brightness-150 transition-all duration-300 text-white py-2 px-4 rounded flex-1 font-medium text-[15px]"
                 >
                   Reset
                 </button>
--- src/views/CryptoInnovationPage.tsx
+++ src/views/CryptoInnovationPage.tsx
@@ -1,33 +1,10 @@
 import MainBg from "../assets/solAdz-bg.png";
-import GetStartedToday from "../assets/get-started-today.svg";
-import LinesRight from "../assets/lines-right.png";
-import BgLeft from "../assets/bg-left-1.png";
+
 import { BottomStats } from "../components/BottomStats";
-import VerticalDash from "../assets/vertical-dash.svg";
 import { Header } from "@/components/Header";
 import { Calculator } from "@/components/Calculator";
-const heroLinks = [
-  {
-    id: 1,
-    title: "wallet tutorial",
-    href: "#"
-  },
-  {
-    id: 2,
-    title: "GUIDE",
-    href: "#"
-  },
-  {
-    id: 3,
-    title: "EXCHANGE",
-    href: "#"
-  },
-  {
-    id: 4,
-    title: "SHOP",
-    href: "#"
-  }
-];
+import { SubHeader } from "@/components/SubHeader";
+import { ContentSection } from "@/components/ContentSection";
 
 export const CryptoInnovationPage = () => {
   return (
@@ -36,56 +13,13 @@ export const CryptoInnovationPage = () => {
 
       <div className="relative z-10">
         {/* Upper sections */}
-
-        <div className=" max-w-7xl mx-auto px-4 md:px-8">
+        <div className="max-w-7xl mx-auto px-4 md:px-8">
           <Header />
-
-          <div className="flex gap-2 md:gap-2 flex-col md:flex-row justify-center md:justify-between items-center">
-            <nav className="text-black flex gap-3 mt-4 md:mt-0">
-              {heroLinks.map((val, index) => (
-                <div key={val.id} className="flex items-center gap-3">
-                  <a
-                    href={val.href}
-                    className="uppercase font-medium text-xs sm:text-sm md:text-base lg:text-2xl hover:text-black/70 transition-all duration-300"
-                  >
-                    {val.title}
-                  </a>
-                  {index !== heroLinks.length - 1 && <img src={VerticalDash} alt="vertical-dash" />}
-                </div>
-              ))}
-            </nav>
-            <button>
-              <img src={GetStartedToday} alt="Get Started Today" />
-            </button>
-          </div>
+          <SubHeader />
         </div>
 
         {/* second section */}
-        <div className="pointer-events-none grid grid-cols-1 lg:grid-cols-3 relative z-10  gap-8">
-          <div className="relative text-white p-8 col-span-2 pb-12 md:pb-16 lg:pb-24">
-            {/* bg image */}
-            <img
-              className="absolute w-full h-full top-0 left-0 pointer-events-none"
-              src={BgLeft}
-              alt=""
-            />
-            <div className="relative z-10 lg:ml-auto mr-[22vw] lg:mr-[18vw] max-w-[558px]">
-              <h2 className="text-[30px] md:text-[40px] lg:text-[60px] poller font-bold mb-4">
-                SolAdz
-              </h2>
-              <h3 className="max-w-[393px] text-[20px] md:text-[30px] lg:text-[40px] leading-[1.5] font-bold">
-                Leading the Way in Crypto Innovation
-              </h3>
-              <p className="text-xs sm:text-sm md:text-[15px] mt-4">
-                Soladz revolutionizes cryptocurrencies. Soladz is a secure, intuitive, and powerful
-                platform for beginners and experts to manage, trade, and grow crypto portfolios
-              </p>
-            </div>
-          </div>
-          <div className="hidden lg:block -ml-[10vw] mt-20 md:pr-8 lg:pr-16">
-            <img src={LinesRight} alt="Lines Right" />
-          </div>
-        </div>
+        <ContentSection />
 
         {/* third calculator section */}
         <Calculator />
